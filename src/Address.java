1b7a564ab46a8e429de8dfcc783ff19ba84aa416
Aisling Boner <55714619+AislingBoner@users.noreply.github.com>
2020-04-19 03:05:21 +0100

    Add files via upload

---
+++ src/Address.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class Address implements Serializable{
+    private String street;
+    
+    public Address(String street){
+        this.street = street;
+    }
+    
+    public String getStreet(){
+        return this.street;
+    }
+}
---
+++ src/Course.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Course implements Serializable{
+    private String name;
+    
+    public Course(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+}
